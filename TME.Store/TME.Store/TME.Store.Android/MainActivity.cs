0fd1ba7f395be5a3640a6c574d60c8dbda602e94
Yevhenii Kyshko <yevhenii.kyshko@vistex.com>
2018-04-03 23:51:16 +0200

    Initial commit. List of Stocks.
    #1

---
+++ TME.Store/TME.Store/TME.Store.Android/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using Android.App;
+using Android.Content.PM;
+using Android.Content.Res;
+using Android.Media;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace TME.Store.Droid
+{
+    [Activity(Label = "TME.Store", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+            string fullSecret;
+            AssetManager assets = this.Assets;
+            using (StreamReader sr = new StreamReader(assets.Open("Secret.txt")))
+            {
+                fullSecret = sr.ReadToEnd();
+            }
+            string[] parts = fullSecret.Split('|');
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App(parts[0], parts[1]));
+        }
+    }
+}
+
---
+++ TME.Store/TME.Store/TME.Store/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using tmeapiwrapper;
+using tmeapiwrapper.Data;
+using tmeapiwrapper.net.Data;
+using Xamarin.Forms;
+
+namespace TME.Store
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage(string secret, string token)
+		{
+			InitializeComponent();
+		    BindingContext = this;
+            GetStocks(secret, token);
+		}
+
+	    private void GetStocks(string secret, string token)
+	    {
+	        try
+	        {
+                ProductsWrapper test = new ProductsWrapper(secret, token);
+
+	            RootObjectResponse rootResponse = test.GetStocks("PL", "EN", new List<string>() { "1WAT-LED-LIGHT", "3CHAZ-LO", "2W08G-E4/51" });
+
+	            string s = rootResponse.Data.ToString();
+	            var castTest = JsonConvert.DeserializeObject<ProductGetStocksResponseData>(s);
+	            ProductsListView.ItemsSource = castTest.ProductList;
+	        }
+	        catch (Exception e)
+	        {
+	            Debug.WriteLine(e);
+	        }
+
+	    }
+	}
+}
